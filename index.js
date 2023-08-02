855ac60da7e57805e5c71fd362275884cc0cbc2a
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2023-08-02 16:20:01 -0400

    Create plugin

---
+++ index.js
@@ -0,0 +1,21 @@
+export default {
+  install: function (app) {
+    app.mixin({
+      beforeCreate: function () {
+        const constants = this.$options.constants;
+        if (constants) {
+          const computeds = {};
+          Object.keys(constants).forEach(function (constantKey) {
+            computeds[constantKey] = function () {
+              return Object.freeze(constants[constantKey]);
+            };
+          });
+          this.$options.computed = {
+            ...this.$options.computed,
+            ...computeds
+          };
+        }
+      },
+    });
+  },
+};

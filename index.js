68206f375a135a682f830ee5892733e57ba43acb
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2023-08-02 16:57:37 -0400

    Add comments

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
+/**
+ * The Vue Constants Plugin which allows adding a constants section to an Options API component.
+ * @type {object}
+ */
 export default {
+  /**
+   * Vue Plugin install function, called by Vue.js. Installs the Constants plugin.
+   *
+   * @param  {Object} app  The Vue app instance.
+   */
   install: function (app) {
+    // Creates a global mixin applied to every component in the app
     app.mixin({
+      // Creates a lifecycle hook in each component
       beforeCreate: function () {
+        // Checks if the component is using the Options API and a constants section exists
         const constants = this.$options.constants;
         if (constants) {
+          // Loops over the constants creating a computed property for each
           const computeds = {};
           Object.keys(constants).forEach(function (constantKey) {
             computeds[constantKey] = function () {
+              // In case the value is not primitive, ensure it is frozen
               return Object.freeze(constants[constantKey]);
             };
           });
+          // Add the computed constatns to the computed properties in the current component
           this.$options.computed = {
             ...this.$options.computed,
             ...computeds
